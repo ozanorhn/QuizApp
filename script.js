5da1668c9bf90aa2f7b2c18803c24eb7f675120f
ozanorhn <ozanorhan60@icloud.com>
2024-09-27 12:33:30 +0200

    Project completed

--- script.js
+++ script.js
@@ -1,157 +1,173 @@
 // Array von Fragen und Antworten für das Quiz
-let questions = [
+const questions = [
     {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right_answer": "3"
+        question: "Wer hat HTML erfunden?",
+        answers: ["Robbie Williams", "Lady Gaga", "Tim Berners-Lee", "Justin Bieber"],
+        correctAnswer: 2
     },
     {
-        "question": "Welches ist das größte Land der Welt nach Fläche?",
-        "answer_1": "Kanada",
-        "answer_2": "China",
-        "answer_3": "USA",
-        "answer_4": "Russland",
-        "right_answer": "4"
+        question: "Welches ist das größte Land der Welt nach Fläche?",
+        answers: ["Kanada", "China", "USA", "Russland"],
+        correctAnswer: 3
     },
     {
-        "question": "Wie viele Planeten hat unser Sonnensystem?",
-        "answer_1": "7",
-        "answer_2": "8",
-        "answer_3": "9",
-        "answer_4": "10",
-        "right_answer": "2"
+        question: "Wie viele Planeten hat unser Sonnensystem?",
+        answers: ["7", "8", "9", "10"],
+        correctAnswer: 1
     },
     {
-        "question": "Wer schrieb das Theaterstück 'Romeo und Julia'?",
-        "answer_1": "Johann Wolfgang von Goethe",
-        "answer_2": "Friedrich Schiller",
-        "answer_3": "William Shakespeare",
-        "answer_4": "Thomas Mann",
-        "right_answer": "3"
+        question: "Wer schrieb das Theaterstück 'Romeo und Julia'?",
+        answers: ["Johann Wolfgang von Goethe", "Friedrich Schiller", "William Shakespeare", "Thomas Mann"],
+        correctAnswer: 2
     },
     {
-        "question": "Welches Element hat das chemische Symbol 'O'?",
-        "answer_1": "Gold",
-        "answer_2": "Silber",
-        "answer_3": "Osmium",
-        "answer_4": "Sauerstoff",
-        "right_answer": "4"
+        question: "Welches Element hat das chemische Symbol 'O'?",
+        answers: ["Gold", "Silber", "Osmium", "Sauerstoff"],
+        correctAnswer: 3
     },
     {
-        "question": "In welchem Jahr fiel die Berliner Mauer?",
-        "answer_1": "1989",
-        "answer_2": "1990",
-        "answer_3": "1987",
-        "answer_4": "1985",
-        "right_answer": "1"
+        question: "In welchem Jahr fiel die Berliner Mauer?",
+        answers: ["1989", "1990", "1987", "1985"],
+        correctAnswer: 0
     },
     {
-        "question": "Welches ist das höchste Gebirge der Welt?",
-        "answer_1": "Rocky Mountains",
-        "answer_2": "Anden",
-        "answer_3": "Himalaya",
-        "answer_4": "Alpen",
-        "right_answer": "3"
+        question: "Welches ist das höchste Gebirge der Welt?",
+        answers: ["Rocky Mountains", "Anden", "Himalaya", "Alpen"],
+        correctAnswer: 2
     },
     {
-        "question": "Wie viele Bundesländer hat Deutschland?",
-        "answer_1": "14",
-        "answer_2": "15",
-        "answer_3": "16",
-        "answer_4": "17",
-        "right_answer": "3"
+        question: "Wie viele Bundesländer hat Deutschland?",
+        answers: ["14", "15", "16", "17"],
+        correctAnswer: 2
     },
     {
-        "question": "Wer malte die Mona Lisa?",
-        "answer_1": "Michelangelo",
-        "answer_2": "Leonardo da Vinci",
-        "answer_3": "Vincent van Gogh",
-        "answer_4": "Pablo Picasso",
-        "right_answer": "2"
+        question: "Wer malte die Mona Lisa?",
+        answers: ["Michelangelo", "Leonardo da Vinci", "Vincent van Gogh", "Pablo Picasso"],
+        correctAnswer: 1
     },
     {
-        "question": "Was ist die Hauptstadt von Australien?",
-        "answer_1": "Sydney",
-        "answer_2": "Melbourne",
-        "answer_3": "Perth",
-        "answer_4": "Canberra",
-        "right_answer": "4"
+        question: "Was ist die Hauptstadt von Australien?",
+        answers: ["Sydney", "Melbourne", "Perth", "Canberra"],
+        correctAnswer: 3
+    },
+    {
+        question: "Welches Land ist bekannt für seine Tulpen?",
+        answers: ["Belgien", "Niederlande", "Frankreich", "Schweden"],
+        correctAnswer: 1
+    },
+    {
+        question: "Welche Farbe hat die Sonne?",
+        answers: ["Blau", "Rot", "Gelb", "Grün"],
+        correctAnswer: 2
+    },
+    {
+        question: "In welchem Jahr fand die erste Mondlandung statt?",
+        answers: ["1965", "1967", "1969", "1971"],
+        correctAnswer: 2
+    },
+    {
+        question: "Welches Land war der erste, der die Atombombe einsetzte?",
+        answers: ["Japan", "Deutschland", "Vereinigte Staaten", "Russland"],
+        correctAnswer: 2
+    },
+    {
+        question: "Welches Land gewann den ersten Fußball-Weltmeistertitel?",
+        answers: ["Italien", "Brasilien", "Uruguay", "Argentinien"],
+        correctAnswer: 2
     }
 ];
 
 let rightQuestions = 0;
-
 let currentQuestion = 0;
+const AUDIO_SUCCESS = new Audio('sounds/success.mp3');
+const AUDIO_FAIL = new Audio('sounds/wrong.mp3');
 
-// Initialisiert das Quiz, zeigt die Anzahl der Fragen und die erste Frage an
+// Initialisiert das Quiz und zeigt die Anzahl der Fragen an
 function init() {
-    document.getElementById('all-questions').innerHTML = questions.length; // Zeigt die Gesamtanzahl der Fragen an
-    showQuestion(); // Ruft die Funktion auf, um die aktuelle Frage anzuzeigen
+    document.getElementById('all-questions').innerHTML = questions.length;
+    showQuestion();
 }
 
 // Zeigt die aktuelle Frage und die Antworten an
 function showQuestion() {
-
-    if (currentQuestion >= questions.length) {
-        document.getElementById(`endScreen`).style = ``;
-        document.getElementById(`questionBody`).style = `display: none;`;
-
-        document.getElementById(`amount-of-Question`).innerHTML = questions.length;
-        document.getElementById(`amount-of-right-Question`).innerHTML = rightQuestions;
+    if (gameIsOver()) {
+        showEndScreen();
     } else {
-
-        let question = questions[currentQuestion]; // Holt die aktuelle Frage aus dem Array
-
-        // Setzt den Text der Frage und Antworten in die entsprechenden HTML-Elemente
-
-        document.getElementById('questiontext').innerHTML = question['question'];
-        document.getElementById('answer_1').innerHTML = question['answer_1'];
-        document.getElementById('answer_2').innerHTML = question['answer_2'];
-        document.getElementById('answer_3').innerHTML = question['answer_3'];
-        document.getElementById('answer_4').innerHTML = question['answer_4'];
-        document.getElementById(`question-number`).innerHTML = currentQuestion + 1;
-        
-
+        updateProgressBar();
+        updateToNextQuestion();
     }
 }
 
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
+function updateProgressBar() {
+    const percent = (currentQuestion / questions.length) * 100;
+    document.getElementById('progress-bar').style.width = `${percent}%`;
+    document.getElementById('progress-bar').innerHTML = `${Math.round(percent)} %`;
+}
+
+function updateToNextQuestion() {
+    const question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question.question;
+    document.getElementById('answer_1').innerHTML = question.answers[0];
+    document.getElementById('answer_2').innerHTML = question.answers[1];
+    document.getElementById('answer_3').innerHTML = question.answers[2];
+    document.getElementById('answer_4').innerHTML = question.answers[3];
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
+}
+
+function showEndScreen() {
+    document.getElementById('endScreen').style.display = '';
+    document.getElementById('questionBody').style.display = 'none';
+
+    document.getElementById('amount-of-Question').innerHTML = questions.length;
+    document.getElementById('amount-of-right-Question').innerHTML = rightQuestions;
+    document.getElementById('header-image').src = './assets/img/images.png';
+}
 
 // Überprüft die gegebene Antwort und gibt das Ergebnis aus
 function answer(selection) {
-    let question = questions[currentQuestion]; // Holt die aktuelle Frage aus dem Array
-    let selectedQuestionNumber = selection.slice(-1); // Extrahiert die Nummer der ausgewählten Antwort (z.B. '1' aus 'answer_1')
-    let idOfRightAnswer = `answer_${question['right_answer']}`;
+    const question = questions[currentQuestion];
+    const selectedQuestionNumber = selection.slice(-1);
+    const idOfRightAnswer = `answer_${question.correctAnswer + 1}`;
 
-    // Vergleicht die ausgewählte Antwortnummer mit der richtigen Antwortnummer
-    if (selectedQuestionNumber == question['right_answer']) {
-        console.log(`Richtige Antwort!!`); // Gibt im Console-Log aus, wenn die Antwort richtig ist
+    if (selectedQuestionNumber == question.correctAnswer + 1) {
+        console.log('Richtige Antwort!!');
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        AUDIO_SUCCESS.play();
         rightQuestions++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
     }
-    document.getElementById(`next-button`).disabled = false;
-
+    document.getElementById('next-button').disabled = false;
 }
 
 function nextQuestion() {
-    currentQuestion++; //zbs. von 0 auf 1
-    document.getElementById(`next-button`).disabled = true;
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
     resetAnswerButtons();
     showQuestion();
+    updateProgressBar();
 }
 
 function resetAnswerButtons() {
-    document.getElementById(`answer_1`).parentNode.classList.remove('bg-danger');
-    document.getElementById(`answer_1`).parentNode.classList.remove('bg-success');
-    document.getElementById(`answer_2`).parentNode.classList.remove('bg-danger');
-    document.getElementById(`answer_2`).parentNode.classList.remove('bg-success');
-    document.getElementById(`answer_3`).parentNode.classList.remove('bg-danger');
-    document.getElementById(`answer_3`).parentNode.classList.remove('bg-success');
-    document.getElementById(`answer_4`).parentNode.classList.remove('bg-danger');
-    document.getElementById(`answer_4`).parentNode.classList.remove('bg-success');
+    for (let i = 1; i <= 4; i++) {
+        const answerButton = document.getElementById(`answer_${i}`);
+        answerButton.parentNode.classList.remove('bg-danger', 'bg-success');
+    }
+}
+
+function restartGame() {
+    document.getElementById('header-image').src = './assets/img/Quiz-Time.jpg';
+    document.getElementById('questionBody').style.display = '';
+    document.getElementById('endScreen').style.display = 'none';
+
+    rightQuestions = 0;
+    currentQuestion = 0;
+    init();
 }
